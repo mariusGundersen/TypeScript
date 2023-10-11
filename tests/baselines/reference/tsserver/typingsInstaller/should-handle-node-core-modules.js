d90e02f4ca3b7f274face78bc85b90b1aade6920
Sheetal Nandi <shkamat@microsoft.com>
2023-10-11 12:14:21 -0700

    Handle unnecessary typings request force just because root files change First unresolved import gets the typing file into cache and sets it as root, This results in scheduling new typing request with that unresolved import missing The result will come back with typing file that is omitted, so root file will change but file will still be part of program so its not really removed from program

--- tests/baselines/reference/tsserver/typingsInstaller/should-handle-node-core-modules.js
+++ tests/baselines/reference/tsserver/typingsInstaller/should-handle-node-core-modules.js
@@ -323,14 +323,6 @@ Info seq  [hh:mm:ss:mss] 	Files (3)
 	/tmp/node_modules/node/index.d.ts Text-1 "\ndeclare module \"net\" {\n    export type n = number;\n}\ndeclare module \"stream\" {\n    export type s = string;\n}"
 	/a/b/app.js SVC-1-1 "// @ts-check\n\nconst net = require(\"net\");\nconst stream = require(\"s tream\");"
 
-
-	../lib/lib.d.ts
-	  Default library for target 'es5'
-	../../tmp/node_modules/node/index.d.ts
-	  Imported via "net" from file 'app.js'
-	app.js
-	  Root file specified for compilation
-
 Info seq  [hh:mm:ss:mss] -----------------------------------------------
 TI:: [hh:mm:ss:mss] Got install request
     {
